d238c0a759507053388bc5ab420504fd16b61b52
Uziel Querendo de Oliveira Junior <uziel.junior@ad.lgsa.com.br>
2020-08-25 16:30:06 -0300

    Versao inicial com Northon

---
+++ CriptoMax/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CriptoMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string varHuman = "Northon";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("jegue" + " " + varHuman);
+            int varInt;
+            string varTexto = Console.ReadLine();
+            if (int.TryParse(varTexto, out varInt))
+            {
+                Console.WriteLine("Converteu inteiro! TOMA!");
+            }
+            else
+            {
+                Console.WriteLine("Não coverteu! Se fuDEU!");
+            }
+            Console.WriteLine(varTexto);
+        }
+
+    }
+}
---
+++ CriptoMax/Quadrado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriptoMax
+{
+    class Quadrado
+    {
+        int lado;
+
+        public Quadrado(int lado)
+        {
+            this.lado = lado;
+        }
+    }
+}
