76af40bf95e852fc14562cef23264a0c1e3087ba
Uziel Querendo de Oliveira Junior <uziel.junior@ad.lgsa.com.br>
2020-08-28 10:24:05 -0300

    Criptografando e descriptografando

---
+++ CriptoMax/Objetos/Criptografia.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace CriptoMax.Objetos
+{
+    class Criptografia
+    {
+        Dictionary<char, int> dicionario;
+        int bitao = 5;
+        public Criptografia()
+        {
+            dicionario = new Dictionary<char, int>();
+            PreencherDicionario();
+        }
+        public string Descriptografar(string fraseBinaria)
+        {
+            string[] vetorString = new string[fraseBinaria.Length/bitao];
+
+            string fraseDescriptografada = "";
+
+            for (int i = 0; i < fraseBinaria.Length/bitao; i++)
+            {
+                vetorString[i] = fraseBinaria.Substring(i * bitao, bitao);
+                
+                int numeroDecimal = Convert.ToInt32(vetorString[i], 2);
+                foreach (KeyValuePair<char,int> significado in dicionario)
+                {
+                    if (significado.Value == numeroDecimal)
+                    {
+                        fraseDescriptografada += significado.Key;
+                    }
+                }
+            }
+            return fraseDescriptografada;
+        }
+        public string Criptografar(char caracter)
+        {
+           
+            return Convert.ToString(dicionario[caracter], 2).PadLeft(bitao, '0');
+        }
+
+        private void PreencherDicionario()
+        {
+            dicionario['a'] = 0;
+            dicionario['b'] = 1;
+            dicionario['c'] = 2;
+            dicionario['d'] = 3;
+            dicionario['e'] = 4;
+            dicionario['f'] = 5;
+            dicionario['g'] = 6;
+            dicionario['h'] = 7;
+            dicionario['i'] = 8;
+            dicionario['j'] = 9;
+            dicionario['k'] = 10;
+            dicionario['l'] = 11;
+            dicionario['m'] = 12;
+            dicionario['n'] = 13;
+            dicionario['o'] = 14;
+            dicionario['p'] = 15;
+            dicionario['q'] = 16;
+            dicionario['r'] = 17;
+            dicionario['s'] = 18;
+            dicionario['t'] = 19;
+            dicionario['u'] = 20;
+            dicionario['v'] = 21;
+            dicionario['w'] = 22;
+            dicionario['x'] = 23;
+            dicionario['y'] = 24;
+            dicionario['z'] = 25;
+            dicionario[' '] = 26;
+        }
+    }
+}
--- CriptoMax/Program.cs
+++ CriptoMax/Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Collections.Generic;
 using System.Collections;
 using System.IO;
+using CriptoMax.Objetos;
 
 namespace CriptoMax
 {
@@ -10,48 +11,27 @@ namespace CriptoMax
     {
         static void Main(string[] args)
         {
-            Dictionary<char, int> dicionarioABC = new Dictionary<char, int>();
 
-            dicionarioABC['a'] = 0;
-            dicionarioABC['b'] = 1;
-            dicionarioABC['c'] = 2;
-            dicionarioABC['d'] = 3;
-            dicionarioABC['e'] = 4;
-            dicionarioABC['f'] = 5;
-            dicionarioABC['g'] = 6;
-            dicionarioABC['h'] = 7;
-            dicionarioABC['i'] = 8;
-            dicionarioABC['j'] = 9;
-            dicionarioABC['k'] = 10;
-            dicionarioABC['l'] = 11;
-            dicionarioABC['m'] = 12;
-            dicionarioABC['n'] = 13;
-            dicionarioABC['o'] = 14;
-            dicionarioABC['p'] = 15;
-            dicionarioABC['q'] = 16;
-            dicionarioABC['r'] = 17;
-            dicionarioABC['s'] = 18;
-            dicionarioABC['t'] = 19;
-            dicionarioABC['u'] = 20;
-            dicionarioABC['v'] = 21;
-            dicionarioABC['w'] = 22;
-            dicionarioABC['x'] = 23;
-            dicionarioABC['y'] = 24;
-            dicionarioABC['z'] = 25;
-            dicionarioABC[' '] = 26;
-
+            Criptografia encriptador = new Criptografia();
+            
             Console.WriteLine("digite uma frase curta");
             string frase = Console.ReadLine().ToLower();
+
+            string fraseCriptografada = "";
 
             char[] fraseChar = frase.ToCharArray();
             Console.WriteLine();
             Console.WriteLine("O texto convertido é:");
             foreach (char letra in fraseChar)
             {
-                Console.Write(Convert.ToString(dicionarioABC[letra], 2).PadLeft(5, '0'));
+                fraseCriptografada += encriptador.Criptografar(letra);
             }
 
-            
+            Console.WriteLine(fraseCriptografada);
+            Console.WriteLine(fraseCriptografada.Length);
+            Console.WriteLine();
+            Console.WriteLine("A frase descriptografada é:");
+            Console.WriteLine(encriptador.Descriptografar(fraseCriptografada));
             
         }
     }
